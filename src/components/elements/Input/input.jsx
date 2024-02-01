d65f7bd98f5e35d99d393b5d85fccf14b339cd49
indra <indralolx@gmail.com>
2024-02-01 11:29:31 +0800

    features:add some data and card

--- src/components/elements/Input/input.jsx
+++ src/components/elements/Input/input.jsx
@@ -1,11 +1,11 @@
 const Input = () => {
-   return (
-      <input
-         type="text"
-         placeholder="Handphone"
-         className="bg-transparent text-slate-200 outline-none px-4 py-2 w-full"
-      />
-   );
+  return (
+    <input
+      type="text"
+      placeholder="Example: Minecraft"
+      className="bg-transparent text-slate-200 outline-none px-4 py-2 w-full"
+    />
+  );
 };
 
 export default Input;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,11 +2,26 @@ import React from "react";
 import Navbar from "../components/fragments/Navbar";
 
 const Home = () => {
-   return (
-      <div>
-         <Navbar />
+  return (
+    <div>
+      <Navbar />
+      <div className="p-4 h-screen">
+        <div className="max-w-56 shadow-md rounded-lg overflow-hidden cursor-pointer">
+          <div className="max-h-60 hover:brightness-75 transition-all ease-linear">
+            <img src="/images/viewfinder.jpg" />
+          </div>
+          <div className="backdrop-blur-[1px] text-white">
+            <h1 className="font-semibold text-lg px-2">View Finder</h1>
+            <h3 className="text-sm px-2">Rp20000</h3>
+            <div className="flex justify-around mt-2 py-1">
+              <button>Beli</button>
+              <button className="flex items-center">Add To Cart</button>
+            </div>
+          </div>
+        </div>
       </div>
-   );
+    </div>
+  );
 };
 
 export default Home;
